84a0e7ea0c3b8c2323e5511dddb32a384426acff
dlthdnjs0517 <thdnjs0517@naver.com>
2025-06-01 23:10:50 +0900

    직원 추가 form을 popup modal로 구현 완료

---
+++ public/orgChart.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const openBtn = document.querySelector('.addLink');
+	const modal = document.getElementById('popupForm');
+	const closeBtn = document.querySelectorAll('.close');
+	if (openBtn && modal) {
+		openBtn.addEventListener('click', function (e) {
+			e.preventDefault(); //a 태그의 기본 동작 막기(페이지 맨 위로 올라가는 동작 방지
+			modal.classList.remove('hidden');
+		});
+	}
+
+	if (modal && closeBtn.length > 0) {
+		closeBtn.forEach(btn => {
+			btn.addEventListener('click', () => {
+				modal.classList.add('hidden');
+			});
+		});
+	}
+});
