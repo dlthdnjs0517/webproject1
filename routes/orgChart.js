9140cc589d238776e98e6755817a7ab74e7b3b32
dlthdnjs0517 <thdnjs0517@naver.com>
2025-06-08 22:56:31 +0900

    데이터를 db에 추가하는 코드 작성

--- routes/orgChart.js
+++ routes/orgChart.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const app = express();
+const router = express.Router();//라우터 객체를 생성
 const path = require('path');
 
 
-const departments = require('../constants/departments');
+const departments = require('./constants/departments');
+const Employee = require('./models/Employee');
 // const addEmployee = require('./models/Employee');
 
 
-
+app.use(express.urlencoded({ extended: true }));
 
 router.get('/', (req, res) => {
 	res.render('orgChart', { departments, title: '(주)백일몽-조직도' });
 })
 
-
+router.post('/addEmployee', async (req, res) => {
+	try {
+		const { name, department, position, email } = req.body;
+		await new Employee({ name, department, position, email }).save();
+		res.redirect('/orgChart');
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).send('직원 데이터 추가 중 오류가 발생했습니다!');
+	}
+})
 
 module.exports = router;
+//작성된 라우터 객체를 저장해서 내보내는 역할을 한다. 바깥에서 사용할 수 있도록
