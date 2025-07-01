39377b94a53a644605f57892220694188b46cc25
dlthdnjs0517 <thdnjs0517@naver.com>
2025-07-01 16:49:29 +0900

    ✅ 메인페이지 스와이퍼 기능 구현 완료!!

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,13 +1,13 @@
 import MainHeader from "../../components/MainHeader/MainHeader";
+import MainSwiper from "../../components/Swiper/MainSwiper";
 
-
-
-function Home(){
-	return(
-		<>
-		<MainHeader/>
-		</>
-	)
+function Home() {
+  return (
+    <>
+      <MainHeader />
+      <MainSwiper />
+    </>
+  );
 }
 
 export default Home;
