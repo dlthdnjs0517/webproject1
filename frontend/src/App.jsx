1e6df972397dfce083bf4592d546df9662c8e0c7
dlthdnjs0517 <thdnjs0517@naver.com>
2025-07-03 17:58:15 +0900

    ✅ 컴포넌트 라우터 연결 코드 작성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import './App.css'
-import Home from './pages/home/home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/home";
 
 // 서버실행을 위해 npm run dev 입력하기
 
-function App(){
-
-  return(
-  <Routes>
-    <Route path='/' element={<Home/>} />
-  </Routes>
-  )
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/orgChart" element={<orgChart />} />
+    </Routes>
+  );
 }
-
 
 export default App;
