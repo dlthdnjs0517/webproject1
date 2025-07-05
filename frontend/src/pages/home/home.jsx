6e0f0f696eb0cff0aba3d6b113f18c1941d6caf5
dlthdnjs0517 <thdnjs0517@naver.com>
2025-07-05 23:21:24 +0900

    자식 컴포넌트(MainSwiper) props(bgImages) 전달

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import MainHeader from "../../components/MainHeader/MainHeader";
 import MainSwiper from "../../components/Swiper/MainSwiper";
+import bg1 from "../../assets/img/bg3.jpg";
+import bg2 from "../../assets/img/bg2.jpg";
+import bg3 from "../../assets/img/bg4.jpg";
 
 function Home() {
+  const bgImages = [bg1, bg2, bg3];
   return (
     <>
       <MainHeader />
-      <MainSwiper />
+      <MainSwiper bgImages={bgImages} />
     </>
   );
 }
