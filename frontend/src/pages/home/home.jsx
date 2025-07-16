e708104349c74b2226fc23af4b31fc3efcf7452d
dlthdnjs0517 <thdnjs0517@naver.com>
2025-07-16 16:46:32 +0900

    ➖swiper 삭제, ➕MainContent로 대체

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,15 +1,11 @@
 import MainHeader from "../../components/MainHeader/MainHeader";
-import MainSwiper from "../../components/Swiper/MainSwiper";
-import bg1 from "../../assets/img/bg3.jpg";
-import bg2 from "../../assets/img/bg2.jpg";
-import bg3 from "../../assets/img/bg4.jpg";
+import MainContent from "../../components/MainContent/MainContent";
 
 function Home() {
-  const bgImages = [bg1, bg2, bg3];
   return (
     <>
       <MainHeader />
-      <MainSwiper bgImages={bgImages} />
+      <MainContent />
     </>
   );
 }
