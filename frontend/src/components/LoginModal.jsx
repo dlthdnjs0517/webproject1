bdb22c64e89e857970aad7352a18c6ac4cad75e7
dlthdnjs0517 <thdnjs0517@naver.com>
2025-07-23 00:36:40 +0900

    ✏️ tailwind의 원활한 활용을 위해 reset.css 내용 삭제, 로그인 모달 UI 수정

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -28,36 +28,36 @@ export default function LoginModal({ isOpen, onClose, onSuccess }) {
   };
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 shadow-md w-full max-w-sm relative">
+    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg p-2 shadow-md w-full max-w-md h-90 relative">
         <button
           onClick={onClose}
-          className="absolute top-3 right-3 text-gray-600 hover:text-black text-xl"
+          className="absolute right-3 text-gray-600 hover:text-black text-xl"
         >
           &times;
         </button>
-        <h2 className="text-xl font-bold text-center mb-4">로그인</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <h2 className="text-xl font-bold text-center m-8">로그인</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
           <input
             type="text"
             placeholder="아이디"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="border px-3 py-2 rounded focus:ring-2 focus:ring-blue-500"
+            className="border px-3 py-2 mx-auto rounded w-1/2 focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="password"
             placeholder="비밀번호"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border px-3 py-2 rounded focus:ring-2 focus:ring-blue-500"
+            className="border px-3 py-2 mx-auto rounded w-1/2 focus:ring-2 focus:ring-blue-500"
           />
           {errorMsg && (
             <p className="text-red-600 text-sm text-center">{errorMsg}</p>
           )}
           <button
             type="submit"
-            className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+            className="mx-auto w-1/2 bg-[#003049] text-white py-2 rounded hover:bg-[#2d93ad]"
           >
             로그인
           </button>
