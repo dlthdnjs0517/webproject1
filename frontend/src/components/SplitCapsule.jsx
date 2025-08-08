0152500ba1d89c4bed5704d6a4a1527cf38f36a5
dlthdnjs0517 <thdnjs0517@naver.com>
2025-08-08 23:31:43 +0900

    ✏️ 캡슐 애니메이션 2번째 시도

--- frontend/src/components/SplitCapsule.jsx
+++ frontend/src/components/SplitCapsule.jsx
@@ -2,10 +2,12 @@ import * as THREE from "three";
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import CapsuleHalf from "./CapsuleHalf";
+import { useThree } from "@react-three/fiber";
 
 export default function SplitCapsule({ isLoggedIn }) {
   const topRef = useRef();
   const bottomRef = useRef();
+  const { camera } = useThree();
 
   const topColor = "#ffffff";
   const bottomColor = isLoggedIn ? "#E67775" : "#344C80";
@@ -14,10 +16,30 @@ export default function SplitCapsule({ isLoggedIn }) {
     if (!topRef.current || !bottomRef.current) return;
 
     const tl = gsap.timeline();
+    tl.set(topRef.current.position, { x: 0, y: 0, z: 0 });
+    tl.set(bottomRef.current.position, { x: 0, y: 0, z: 0 });
+    tl.set(topRef.current.rotation, { x: Math.PI / 2, y: 0, z: 0 });
+    tl.set(bottomRef.current.rotation, { x: Math.PI / 2, y: 0, z: 0 });
 
-    tl.set(topRef.current.position, { y: 0 }).set(bottomRef.current.position, {
-      y: 0,
-    });
+    // 양옆으로 분리
+    tl.to(topRef.current.position, { z: 2, duration: 1 }, 0);
+    tl.to(bottomRef.current.position, { z: -2, duration: 1 }, 0);
+    // tl.to(topRef.current.rotation, { y: Math.PI / 6 }, 0);
+    // tl.to(bottomRef.current.rotation, { y: -Math.PI / 6 }, 0);
+
+    // 카메라 이동도 함께
+    // tl.to(
+    //   camera.position,
+    //   {
+    //     x: 0,
+    //     y: 6,
+    //     z: 0,
+    //     duration: 1.2,
+    //     ease: "power2.out",
+    //     onUpdate: () => camera.lookAt(8, 2, 0),
+    //   },
+    //   0,
+    // );
   }, [isLoggedIn]);
 
   return (
