a880f348b35b199fe8f0c2bb6376d9662fd78f44
dlthdnjs0517 <thdnjs0517@naver.com>
2025-08-11 19:43:50 +0900

    ✅ 캡슐 절반이 쪼개지는 애니메이션 효과 적용 성공

--- frontend/src/components/SplitCapsule.jsx
+++ frontend/src/components/SplitCapsule.jsx
@@ -16,36 +16,31 @@ export default function SplitCapsule({ isLoggedIn }) {
     if (!topRef.current || !bottomRef.current) return;
 
     const tl = gsap.timeline();
-    tl.set(topRef.current.position, { x: 0, y: 0, z: 0 });
-    tl.set(bottomRef.current.position, { x: 0, y: 0, z: 0 });
-    tl.set(topRef.current.rotation, { x: Math.PI / 2, y: 0, z: 0 });
-    tl.set(bottomRef.current.rotation, { x: Math.PI / 2, y: 0, z: 0 });
 
-    // 양옆으로 분리
+    //양옆으로 분리
     tl.to(topRef.current.position, { z: 2, duration: 1 }, 0);
     tl.to(bottomRef.current.position, { z: -2, duration: 1 }, 0);
-    // tl.to(topRef.current.rotation, { y: Math.PI / 6 }, 0);
-    // tl.to(bottomRef.current.rotation, { y: -Math.PI / 6 }, 0);
+    tl.to(topRef.current.rotation, { y: Math.PI / 6 }, 0);
+    tl.to(bottomRef.current.rotation, { y: -Math.PI / 6 }, 0);
 
-    // 카메라 이동도 함께
-    // tl.to(
-    //   camera.position,
-    //   {
-    //     x: 0,
-    //     y: 6,
-    //     z: 0,
-    //     duration: 1.2,
-    //     ease: "power2.out",
-    //     onUpdate: () => camera.lookAt(8, 2, 0),
-    //   },
-    //   0,
-    // );
+    //카메라 이동도 함께
+    tl.to(
+      camera.position,
+      {
+        x: 0,
+        y: 6,
+        z: 0,
+        duration: 2.5,
+        onUpdate: () => camera.lookAt(0, 0, 0),
+      },
+      0
+    ).set(camera.position, { x: 0, y: 0, z: 0 });
   }, [isLoggedIn]);
 
   return (
     <>
-      <CapsuleHalf ref={topRef} isTop={true} color={topColor} />
-      <CapsuleHalf ref={bottomRef} isTop={false} color={bottomColor} />
+      <CapsuleHalf ref={topRef} isTop={true} color={topColor} axis="z" />
+      <CapsuleHalf ref={bottomRef} isTop={false} color={bottomColor} axis="z" />
     </>
   );
 }
