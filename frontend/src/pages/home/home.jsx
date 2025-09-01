667011149e06811f53b2a684952509e1e5559619
dlthdnjs0517 <thdnjs0517@naver.com>
2025-09-01 15:41:33 +0900

    ➖프로젝트에 활용되지 않는 파일 삭제에 따른 코드 수정

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,12 +1,10 @@
 import MainHeader from "../../components/MainHeader/MainHeader";
-import MainContent from "../../components/MainContent/MainContent";
-import CapsuleContent from "../../components/CapsuleContent";
+import CapsuleContent from "../../components/Capsule/CapsuleContent";
 
 function Home() {
   return (
     <>
       <MainHeader />
-      {/* <MainContent /> */}
       <CapsuleContent />
     </>
   );
