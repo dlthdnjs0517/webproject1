d08f6fd9f1bca680a10f82c3234fdf8d0c32d7f5
dlthdnjs0517 <thdnjs0517@naver.com>
2025-09-15 16:14:52 +0900

    ⛔️ app.css 삭제에 따른 import 코드 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/home/home";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
