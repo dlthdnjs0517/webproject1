25519f2a2f14f5224b1c02b5b8fd110595164491
dlthdnjs0517 <thdnjs0517@naver.com>
2025-10-20 15:03:03 +0900

    ➕ 첫번째 페이지 bg를 style로 지정, css 삭제

--- frontend/src/components/Capsule/CapsuleContent.jsx
+++ frontend/src/components/Capsule/CapsuleContent.jsx
@@ -3,14 +3,21 @@ import { useSelector } from "react-redux";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
 import SplitCapsule from "./SplitCapsule";
-import "./CapsuleContent.css";
 
 export default function CapsuleContent() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const sectionRef = useRef(null);
 
   return (
-    <section ref={sectionRef} style={{ height: "100vh", width: "100vw" }}>
+    <section
+      ref={sectionRef}
+      style={{
+        height: "100vh",
+        width: "100vw",
+        background:
+          "radial-gradient(circle, rgba(241, 225, 242, 1) 0%, rgba(68, 118, 235, 1) 75%, rgba(93, 132, 222, 1) 100%)",
+      }}
+    >
       <Canvas
         camera={{ position: [8, 0, 0], fov: 75 }}
         gl={{ localClippingEnabled: true }}
